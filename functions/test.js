44fe6330432e05919192a70be80e780803dd9862
Naima Malik <naimamaria.malik@medieinstitutet.se>
2021-06-16 12:52:18 +0200

    Set up submit function.

--- functions/test.js
+++ functions/test.js
@@ -1,16 +1,16 @@
-const functions = require("firebase-functions");
+//const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 
-const clientId = "538930344767-95aureeie4sjajafsdisadkuqr9arf2i.apps.googleusercontent.com"
-const clientSecret = "l-9VhO019MChH4RPYLOS4o5r"
+const clientId = "1025842671920-07461gu02ioqvt4ai539lq52pimbk4n2.apps.googleusercontent.com"
+const clientSecret = "9iMieygoMGBMaEBVx_gkiUMg"
 const redirectUrl = "https://developers.google.com/oauthplayground"
 const refreshToken = "1//04yR2ar02OTjTCgYIARAAGAQSNwF-L9Irp99urvprfFDthB198VzPHeLBtFJaUIrAvlWgpXg8B-SOUpcHceybWGWV_CkIcLZNdj0"
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onCall(async (data, context) => {
+async function poop() {
   // let testAccount = await nodemailer.createTestAccount();
   const oauth = new google.auth.OAuth2(clientId, clientSecret, redirectUrl);
   oauth.setCredentials ({ refresh_token: refreshToken });
@@ -28,7 +28,7 @@ exports.helloWorld = functions.https.onCall(async (data, context) => {
     service: 'gmail',
     auth: {
       type: "OAuth2",
-      user: 'saadist@gmail.com',
+      user: 'naima.maria.malik@gmail.com',
       clientId,
       clientSecret,
       refreshToken,
@@ -37,8 +37,8 @@ exports.helloWorld = functions.https.onCall(async (data, context) => {
   });
 
   let info = await transporter.sendMail({
-    from: '"Naima Malik" <naima.maria.malik@gmail.com>', // sender address
-    to: "noma_m@hotmail.com, saadist@gmail.com", // list of receivers
+    from: '"Saad Ahmed" <saadist@gmail.com>', // sender address
+    to: "naima.maria.malik@gmail.com", // list of receivers
     subject: "Hello sexy", // Subject line
     text: "You so bad!", // plain text body
     html: "<b>I so bad!</b>", // html body
@@ -48,4 +48,6 @@ exports.helloWorld = functions.https.onCall(async (data, context) => {
 
   //functions.logger.info("Hello logs!", {structuredData: true});
   return nodemailer.getTestMessageUrl(info);
-});
+}
+
+poop()
--- public/index.js
+++ public/index.js
@@ -1,6 +1,28 @@
-const addMessage = firebase.functions().httpsCallable('helloWorld');
+//const addMessage = firebase.functions().httpsCallable('helloWorld');
 
-const sendEmail = async () => {
-    const res = await addMessage({ text: "Noma" })
-    console.log(res)
+const sendEmail = async (e) => {
+    e.preventDefault()
+    const firstName = document.querySelector('#firstName').value;
+    const lastName = document.querySelector('#lastName').value;
+    const email = document.querySelector('#email').value;
+    const message = document.querySelector('#message').value;
+    const subject = encodeURIComponent(document.querySelector('#subject').value || 'Consultation');
+    let body = ''
+    if (firstName) body += `First name: ${firstName}\n`
+    if (lastName) body += `Last name: ${lastName}\n`
+    if (email) body += `Email: ${email}\n`
+    if (message) body += `\n${message}\n`
+    body = encodeURIComponent(body)
+
+    window.location.href = `mailto:bringtolightcounselling@gmail.com?subject=${subject}&body=${body}`
+
+    document.querySelector('#firstName').value = '';
+    document.querySelector('#lastName').value = '';
+    document.querySelector('#email').value = '';
+    document.querySelector('#message').value = '';
+    document.querySelector('#subject').value = ''
+}
+
+const bookConsultation = () => {
+    window.location.href = `mailto:bringtolightcounselling@gmail.com?subject=Consultation`
 }
